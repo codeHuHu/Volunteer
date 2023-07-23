a505343bf89646a02c6dcb0a274c3aad881d53f8
Cliqin <1113764188@qq.com>
2023-07-23 11:37:49 +0800

    创建队伍的完善

--- pages/teamdetail/teamdetail.js
+++ pages/teamdetail/teamdetail.js
@@ -1,19 +1,34 @@
 // pages/teamdetail/teamdetail.js
-const app=getApp()	
+const app = getApp()
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		info:'',
+		teamName:'广州市交通运输志愿服务支队',
+		members:[6666,['张三',18319093951],['李四',18319093951]],
+		serviceNumber:'166',
+		volunteerTime:'999.8'
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad(options) {
-
+		if(options.info){
+			let info = JSON.parse(decodeURIComponent(options.info))
+      console.log(info)
+      this.setData({
+        teamLeader:info['teamLeader'],
+        teamName : info['teamName'],
+        members:info['teamMembers'],
+        volunteerTime:info['volunteerTime'],
+        teamIntro:info['teamIntro'],
+        mail:info['mail']
+      })
+		}
 	},
 
 	/**
@@ -63,32 +78,30 @@ Page({
 	 */
 	onShareAppMessage() {
 
-  },
-  join()
-  {
-    if (app.globalData.userInfo == null) {
-      
-      wx.showModal({
+	},
+	join() {
+
+		console.log(app.globalData.userInfo)
+
+		if (app.globalData.userInfo == null) {
+			wx.showModal({
 				title: '提示',
 				content: '你未注册，请前往注册认证',
-				success(res){
-          if(res.confirm)
-          {
+				success(res) {
+					if (res.confirm) {
 						console.log("成功")
 						wx.navigateTo({
 							url: '/pages/home/home',
 						})
-					}
-					else {
-						
+					} else {
+
 					}
 				}
 			})
-	}
-		else {
-		wx.showToast({
-			title: '申请成功，等待管理员验证通过',
-		})
+		} else {
+			wx.showToast({
+				title: '申请成功，等待管理员验证通过',
+			})
 		}
-}
+	}
 })
