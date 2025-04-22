3bf7000b795adeda0e3bfdbb212e2958f0b20ff4
xiaomihu69 <2559221382@qq.com>
2025-04-22 22:37:46 +0800

    3

--- pages/PersonalCenter/newActivity/newActivity.js
+++ pages/PersonalCenter/newActivity/newActivity.js
@@ -290,6 +290,7 @@ Page({
 					}
 					console.log(1)
 					console.log(2)
+					console.log(3)
 					const tmpdata = JSON.parse(res.data);
 					const data = tmpdata.data
 					console.log("data= ", data)
