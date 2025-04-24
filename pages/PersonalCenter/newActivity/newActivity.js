0d25df1ce576a8325de80572382f91a5d3c059cb
xiaomihu69 <2559221382@qq.com>
2025-04-24 21:24:51 +0800

    改

--- pages/PersonalCenter/newActivity/newActivity.js
+++ pages/PersonalCenter/newActivity/newActivity.js
@@ -293,6 +293,7 @@ Page({
 					console.log(3)
 					const tmpdata = JSON.parse(res.data);
 					const data = tmpdata.data
+					console.log("已修改")
 					console.log("data= ", data)
 					if (fileName) {
 						let tmp = {
