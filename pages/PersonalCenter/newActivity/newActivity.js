c4ff1fba2a4feeae183e24a131ea856f40f73301
codeHuHu <115488506+codeHuHu@users.noreply.github.com>
2025-04-24 21:58:07 +0800

    Revert "Test"

--- pages/PersonalCenter/newActivity/newActivity.js
+++ pages/PersonalCenter/newActivity/newActivity.js
@@ -289,10 +289,8 @@ Page({
 						})
 					}
 
-					console.log("出错")
 					const tmpdata = JSON.parse(res.data);
 					const data = tmpdata.data
-					console.log("已修改")
 					console.log("data= ", data)
 					if (fileName) {
 						let tmp = {
